cfb896517150c0d401917d203bb7cb6f155a4c66
Jeremy <jeremy.debelleix@gmail.com>
2015-05-13 15:33:50 +0200

    class humain + creation histoire

--- src/Humain.java
+++ src/Humain.java
@@ -11,5 +11,31 @@ public class Humain
 		this.nom = unNom;
 		this.boissonFavorite = uneBoissonFavorite;
 	}
-
+	public Humain(String unNom)
+	{
+		this.nom = unNom;
+	}
+	
+	public String parler(String texte)
+	{
+		return this.nom + " - " + texte; 
+	}
+	
+	
+	public String quelEstTonNom()
+	{
+		return this.nom;
+	}
+	public String quelleEstTaBoisson()
+	{
+		return this.boissonFavorite;
+	}
+	public String presentation()
+	{
+		return "Bonjour, mon nom est "+quelEstTonNom()+" et ma boisson préferée est : "+quelleEstTaBoisson();
+	}
+	public String boire ()
+	{
+		return "Ah un bon verre de "+quelleEstTaBoisson();
+	}
 }
