15f41ac2552a64c12d08d71d8371d40dab13cbab
MonarqueSupreme <g.guillaume91350@gmail.com>
2015-05-13 15:00:44 +0200

    Creation de la classe Humain et de son constructeur (Humain)

---
+++ src/Humain.java
@@ -0,0 +1,15 @@
+
+public class Humain 
+{
+	private String nom;
+	private String boissonFavorite = "Eau";
+	
+	
+	
+	public Humain(String unNom, String uneBoissonFavorite)
+	{
+		this.nom = unNom;
+		this.boissonFavorite = uneBoissonFavorite;
+	}
+
+}
